6257102534432e22e86253c35fc98d147a338b4b
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-10-12 16:04:37 +0900

    first commit

---
+++ DurableFunctionsHttpStart/index.ts
@@ -0,0 +1,16 @@
+﻿import * as df from "durable-functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpStart: AzureFunction = async function (context: Context, req: HttpRequest): Promise<any> {
+    const client = df.getClient(context);
+
+    /* client.startNewでオーケストレーション開始 */ 
+    const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    /* client.createCheckStatusResponseでHTTP応答が返される */ 
+    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
+
+export default httpStart;
---
+++ HelloOrchestrator/index.ts
@@ -0,0 +1,42 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+    const outputs = [];
+    
+    /* ひとつづつ実行する場合 */ 
+    // // Replace "Hello" with the name of your Durable Activity Function.
+    // outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
+    // outputs.push(yield context.df.callActivity("Hello", "Seattle"));
+    // outputs.push(yield context.df.callActivity("Hello", "London"));
+    // // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    // return outputs;
+
+
+    /* 一括で実行する場合 */ 
+    // Replace "Hello" with the name of your Durable Activity Function.
+    outputs.push(context.df.callActivity("Hello", "Tokyo"));
+    outputs.push(context.df.callActivity("Goodbye", "Seattle"));
+    outputs.push(context.df.callActivity("Hello", "London"));
+
+    // returns ["Hello Tokyo!", "Goodbye Seattle!", "Hello London!"]
+    const results = yield context.df.Task.all(outputs);
+
+    
+    return results;
+
+    
+});
+
+export default orchestrator;
+
